8202a79b9a6c047eb39e171f89bf5c499a99714d
Richard G Baker <rich.baker@aexp.com>
2024-12-03 14:54:37 +0000

    Day 3

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,29 @@
+
+val mulRegex = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".toRegex()
+
+fun main() {
+    val text = Any::class::class.java.getResource("/day3.txt")?.readText()?.replace("\r\n", "")
+    println(part1(text!!))
+    println(part2(text))
+}
+
+fun part1(text: String): Int {
+    var result = 0
+    mulRegex.findAll(text).forEach { match ->
+        val (a, b) = match.destructured
+        result += a.toInt() * b.toInt()
+    }
+    return result
+}
+
+fun part2(text: String): Int {
+    var result = 0
+    val activeSections = "(?:do\\(\\)|^).*?(?:don't\\(\\)|\$)".toRegex()
+    activeSections.findAll(text).forEach { section ->
+        mulRegex.findAll(section.value).forEach { match ->
+            val (a, b) = match.destructured
+            result += a.toInt() * b.toInt()
+        }
+    }
+    return result
+}
