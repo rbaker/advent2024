25a6870bef27c4f12b7a47098748e2a260e5fdd6
Richard G Baker <rich.baker@aexp.com>
2024-12-11 17:51:15 +0000

    day 11 complete

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -1,15 +1,29 @@
 fun main() {
-    var numbers = Any::class::class.java.getResource("/day11.txt")?.readText()?.split(" ")
-    for (i in 1..25) {
-        val nextArray = mutableListOf<String>()
-        numbers!!.forEach {
-            if (it == "0") nextArray.add("1")
-            else if (it.length % 2 == 0) {
-                nextArray.add(it.substring(0, it.length / 2))
-                nextArray.add((it.substring(it.length / 2, it.length)).toLong().toString())
-            } else nextArray.add((it.toLong() * 2024).toString())
-        }
-        numbers = nextArray
+    val numbers = Any::class::class.java.getResource("/day11.txt")?.readText()?.split(" ")
+    println(getStoneCount(numbers!!, 25))
+    println(getStoneCount(numbers, 75))
+}
+
+fun getStoneCount(numbers: List<String>, iterations: Int): Long {
+    var counts = mutableMapOf<String, Long>()
+    numbers.forEach {
+        counts[it] = counts.getOrDefault(it, 0L) + 1L
     }
-    println(numbers?.size)
+    repeat(iterations) {
+        val nextCounts = mutableMapOf<String, Long>()
+        counts.forEach {
+            when {
+                it.key == "0" -> nextCounts["1"] = nextCounts.getOrDefault("1", 0L) + it.value
+                it.key.length % 2 == 0 -> {
+                    val firstHalf = it.key.substring(0, it.key.length / 2)
+                    val secondHalf = it.key.substring(it.key.length / 2, it.key.length).toLong().toString()
+                    nextCounts[firstHalf] = nextCounts.getOrDefault(firstHalf, 0L) + it.value
+                    nextCounts[secondHalf] = nextCounts.getOrDefault(secondHalf, 0L) + it.value
+                }
+                else -> nextCounts[(it.key.toLong() * 2024).toString()] = nextCounts.getOrDefault((it.key.toLong() * 2024).toString(), 0L) + it.value
+            }
+        }
+        counts = nextCounts
+    }
+    return counts.values.sum()
 }
