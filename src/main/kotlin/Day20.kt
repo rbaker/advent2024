ca79d1180fb56acaea1ed2496b59531512d27a50
rbaker <richardgbaker@yahoo.co.uk>
2024-12-21 02:03:56 +0000

    removed unnecessary code

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -5,7 +5,7 @@ fun main() {
     val start = Point(0,0)
     val end = Point(0,0)
     val walls = mutableListOf<Point>()
-    val lines = Any::class::class.java.getResource("/Day20.txt")?.readText()?.lines()?.map { it.toCharArray() }!!
+    val lines = Any::class::class.java.getResource("/day20.txt")?.readText()?.lines()?.map { it.toCharArray() }!!
     lines.forEachIndexed { i, l ->
         l.forEachIndexed { j, c ->
             when (c) {
@@ -23,7 +23,7 @@ fun main() {
 fun getCheats(path: List<Point>, searchRadius: Int): Int {
     var result = 0
     path.forEachIndexed { i, c ->
-        val a = path.filter { abs(it.x - c.x) + abs(it.y - c.y) <= searchRadius }.map { path.indexOf(it) }.sorted()
+        val a = path.filter { abs(it.x - c.x) + abs(it.y - c.y) <= searchRadius }.map { path.indexOf(it) }
         a.subList(a.indexOf(i), a.size).forEach { pos ->
             val save = pos - i
             val manhattan = abs(path[pos].x - path[i].x) + abs(path[pos].y - path[i].y)
